985117b4402e48d3972aad3988dde2ba09edecd7
mats12o <mats12o.pruess@outlook.de>
2026-06-13 21:54:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("contextmenu", function(e){
+e.preventDefault();
+});
+
+document.addEventListener("keydown", function(e){
+if(
+e.key === "F12" ||
+(e.ctrlKey && e.shiftKey && e.key === "I") ||
+(e.ctrlKey && e.shiftKey && e.key === "J") ||
+(e.ctrlKey && e.key === "U")
+){
+e.preventDefault();
+}
+});
